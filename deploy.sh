f3111344eeb0ad2cbd4eb566dbf84d0664633f2b
Stevenson <istevenson@dstsystems.com>
2018-01-22 15:35:28 -0600

    general updates

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,8 @@ bundle exec jekyll build
 cd ${HTML_FOLDER}
 
 # config
-git config --global user.email "nandomoreira.me@gmail.com"
-git config --global user.name "Fernando Moreira"
+git config --global user.email "lulucrewlily@gmail.com"
+git config --global user.name "Imon Stevenson"
 
 # deploy
 git init
